bef1e56c3fb454d7898a10a08adc0ccdffa6e1e6
Chamith Harshamal <chamithweerasinghe1@gmail.com>
2025-07-31 15:32:02 +0530

    Update server CORS configuration to allow all origins for API requests

--- server/main.py
+++ server/main.py
@@ -7,10 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 model_name = "flax-community/t5-recipe-generation"
